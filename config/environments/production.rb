b69ed6cd4576d02dce571d57559a128d0bc8b01a
victor Valencia <vicvalen@gmail.com>
2016-04-17 02:34:28 -0700

    Amazon not region

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,6 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
   :storage => :s3,
-  :s3_region => 'us-west-2', 
   :s3_credentials => {
     :bucket => 'onixmediadesign',
     :access_key_id => "AKIAJHTDANPCGOYXD2GA",
