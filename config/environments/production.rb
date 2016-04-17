927fd7a04ddda6cc6268cdca8d207175153ebc57
victor Valencia <vicvalen@gmail.com>
2016-04-17 00:35:23 -0700

    Amazon Region

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,7 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
   :storage => :s3,
+  :s3_region => ENV['AWS_REGION'],
   :s3_credentials => {
     :bucket => 'onixmediaimages',
     :access_key_id => 'AKIAJHTDANPCGOYXD2GA',
