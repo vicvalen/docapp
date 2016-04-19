8b4a923496586cdfa8feabec9c8c109c1ceb13b9
victor Valencia <vicvalen@gmail.com>
2016-04-19 15:49:07 -0700

    estilos

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,6 +1,6 @@
 class Doctor < ActiveRecord::Base
 	belongs_to :user
-	has_many :tasks
+	has_many :tasks, inverse_of: :doctor
 	accepts_nested_attributes_for :tasks, reject_if: :all_blank, allow_destroy: true
 	has_attached_file :image, styles: { medium: "300x300>"}
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ActiveRecord::Base
 	belongs_to :doctor
 	validates :doctor, presence: true
+	validates  :description, presence: true
 end
